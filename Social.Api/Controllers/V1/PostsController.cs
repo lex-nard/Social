44ac120b8c74a3a05cf1a8822cc36e13d969cc03
lex-nard <felipe_osti98@hotmail.com>
2022-07-13 23:29:37 -0300

    FEAT: Project initialization
    Configuring, adding swagger and versioning

---
+++ Social.Api/Controllers/V1/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Social.Domain.Models;
+
+namespace Social.Api.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var post = new Post { Id = id, Text = "May the Force be with you" };
+
+            return Ok(post);
+        }
+    }
+}
---
+++ Social.Api/Controllers/V2/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Social.Domain.Models;
+
+namespace Social.Api.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var post = new Post { Id = id, Text = "Are you expecting something to happen?" };
+
+            return Ok(post);
+        }
+    }
+}
